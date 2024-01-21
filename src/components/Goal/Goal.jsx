e4c908a8e4b91c804517f9c533f8f44474c2cc9e
KRVitalik <karmelitavitalik@gmail.com>
2024-01-21 12:20:08 +0200

    Update Goal.jsx

--- src/components/Goal/Goal.jsx
+++ src/components/Goal/Goal.jsx
@@ -13,30 +13,30 @@ const Goal = () => {
       />
       <P>What are your goals?</P>
       <ItemContainer>
-        <Link to={'/measure'}>
-          <ItemContentLink>
+        <ItemContentLink>
+          <Link to={'/measure'}>
             <p>Lose Weight</p>
             <Icon iconId="group-1" h={172} />
-          </ItemContentLink>
-        </Link>
-        <Link to={'/measure'}>
-          <ItemContentLink>
+          </Link>
+        </ItemContentLink>
+        <ItemContentLink>
+          <Link to={'/measure'}>
             <p>Gain Muscle</p>
             <Icon iconId="group-2" h={172} />
-          </ItemContentLink>
-        </Link>
-        <Link to={'/measure'}>
-          <ItemContentLink>
+          </Link>
+        </ItemContentLink>
+        <ItemContentLink>
+          <Link to={'/measure'}>
             <p>Develop healthy habits</p>
             <Icon iconId="group-3" h={172} />
-          </ItemContentLink>
-        </Link>
-        <Link to={'/measure'}>
-          <ItemContentLink>
+          </Link>
+        </ItemContentLink>
+        <ItemContentLink>
+          <Link to={'/measure'}>
             <p>Develop healthy habits</p>
             <Icon iconId="group-4" h={172} />
-          </ItemContentLink>
-        </Link>
+          </Link>
+        </ItemContentLink>
       </ItemContainer>
     </div>
   );
