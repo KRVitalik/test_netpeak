5e7946166c9e56c4eaa9c76ceecb0b0755fe043d
KRVitalik <karmelitavitalik@gmail.com>
2024-01-21 14:28:50 +0200

    Update Behaviors.jsx

--- src/components/pages/Behaviors/Behaviors.jsx
+++ src/components/pages/Behaviors/Behaviors.jsx
@@ -16,27 +16,27 @@ const Behaviors = () => {
       <AfterTitleText text={`We all have them! Which are yours?`} />
       <BehaviorsGrid>
         <BehaviorsGridItem>
-          <Icon iconId="moon" h={30} />
+          <Icon iconId="moon" h={30} w={30} />
           <BehaviorsGridItemText>I don't rest enough</BehaviorsGridItemText>
         </BehaviorsGridItem>
         <BehaviorsGridItem>
-          <Icon iconId="donut" h={30} />
+          <Icon iconId="donut" h={30} w={30} />
           <BehaviorsGridItemText>I have a sweet tooth</BehaviorsGridItemText>
         </BehaviorsGridItem>
         <BehaviorsGridItem>
-          <Icon iconId="soda" h={30} />
+          <Icon iconId="soda" h={30} w={30} />
           <BehaviorsGridItemText>I have too much soda</BehaviorsGridItemText>
         </BehaviorsGridItem>
         <BehaviorsGridItem>
-          <Icon iconId="salt" h={30} />
+          <Icon iconId="salt" h={30} w={30} />
           <BehaviorsGridItemText>I eat many salty foods</BehaviorsGridItemText>
         </BehaviorsGridItem>
         <BehaviorsGridItem>
-          <Icon iconId="pizza" h={30} />
+          <Icon iconId="pizza" h={30} w={30} />
           <BehaviorsGridItemText>I enjoy midnight snacks</BehaviorsGridItemText>
         </BehaviorsGridItem>
         <BehaviorsGridItem>
-          <Icon iconId="cross" h={30} />
+          <Icon iconId="cross" h={30} w={30} />
           <BehaviorsGridItemText>None of the above</BehaviorsGridItemText>
         </BehaviorsGridItem>
       </BehaviorsGrid>
